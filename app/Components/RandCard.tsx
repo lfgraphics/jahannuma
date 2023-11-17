bb00625c6cd303a3eb7c706ff13d043d2be2764e
Taha Kazmi <tahakazmi143@gmail.com>
2023-11-18 02:14:57 +0530

    fixed the err

--- app/Components/RandCard.tsx
+++ app/Components/RandCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import * as data from "../Ghazlen/data";
+// import * as data from "../Ghazlen/data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload, faShare } from "@fortawesome/free-solid-svg-icons";
 import html2canvas from "html2canvas";
@@ -9,15 +9,65 @@ import { ArrowDown } from "react-feather";
 import Link from "next/link";
 
 interface Shaer {
-  shaer: string;
-  sherHead: string[];
-  wholeSher: string[];
+  fields: {
+    ghazal: string[];
+    ghazalHead: string[];
+    shaer: string;
+  };
+  id: string;
+  createdTime: string;
 }
 
 const RandCard: React.FC<{}> = () => {
-  const dataItems: Shaer[] = data.getAllShaers();
-  const randomIndex = Math.floor(Math.random() * dataItems.length);
-  const randomData = dataItems[randomIndex];
+  const [dataItems, setDataItems] = useState<Shaer[]>([]);
+  const [randomData, setRandomData] = useState<Shaer | null>(null);
+  const fetchData = async () => {
+    try {
+      const API_KEY =
+        "patpWvd49NVJhHOVr.73ebeea33c6733900c098b73f0d71a60114061896d4051a451e7e24d59351cef";
+      const BASE_ID = "appvzkf6nX376pZy6";
+      const TABLE_NAME = "ghazlen";
+
+      let url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
+
+      const headers = {
+        Authorization: `Bearer ${API_KEY}`,
+      };
+
+      const response = await fetch(url, { method: "GET", headers });
+      const result = await response.json();
+
+      const records = result.records || [];
+      // Convert ghazal and ghazalHead fields to arrays
+      const formattedRecords = records.map(
+        (record: { fields: { ghazal: string; ghazalHead: string } }) => ({
+          ...record,
+          fields: {
+            ...record.fields,
+            ghazal: record.fields.ghazal.split("\n"),
+            ghazalHead: record.fields.ghazalHead.split("\n"),
+          },
+        })
+      );
+
+      setDataItems(formattedRecords);
+      console.log(formattedRecords);
+
+      // console.log(filteredRecord)
+    } catch (error) {
+      console.error(`Failed to fetch data: ${error}`);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  useEffect(() => {
+    // Generate randomData only if dataItems is not empty
+    if (dataItems.length > 0) {
+      const randomIndex = Math.floor(Math.random() * dataItems.length);
+      setRandomData(dataItems[randomIndex]);
+    }
+  }, [dataItems]);
 
   // Define handleDownload and handleShareClick functions here
   const handleDownload = (elementId: string) => {
@@ -56,9 +106,9 @@ const RandCard: React.FC<{}> = () => {
       if (navigator.share) {
         navigator
           .share({
-            title: shaerData.shaer,
+            title: shaerData.fields.shaer,
             text:
-              shaerData.sherHead.map((line) => line).join("\n") +
+              shaerData.fields.ghazalHead.map((line) => line).join("\n") +
               `\nFound this on Jahan Numa website\nCheckout there webpage here>>\n `, // Join sherHead lines with line breaks
 
             url: window.location.href + `/#${id}`,
@@ -87,10 +137,10 @@ const RandCard: React.FC<{}> = () => {
   return (
     <div className="justify-center flex flex-col items-center m-4">
       <h4
-        className="text-xl m-4 font-semibold text-gray-600 tracking-[5px]"
+        className="text-xl m-4 font-semibold text-gray-600 "
         // style={{ letterSpacing: "5px" }}
       >
-        Random sher
+        ایک شعر
       </h4>
       {!insideBrowser && <Loader></Loader>}
       {insideBrowser && (
@@ -98,15 +148,15 @@ const RandCard: React.FC<{}> = () => {
           id={"sherCard"}
           className="bg-white p-4 rounded-sm w-[95vw] justify-center flex flex-col items-center"
         >
-          <Link href={`/Shaer/${randomData.shaer}`}>
+          <Link href={`/Shaer/${randomData?.fields.shaer}`}>
             <h2
               className="text-black text-3xl mb-2"
               style={{ lineHeight: "normal" }}
             >
-              {randomData.shaer}
+              {randomData?.fields.shaer}
             </h2>
           </Link>
-          {randomData.sherHead.map((line, index) => (
+          {randomData?.fields.ghazalHead.map((line, index) => (
             <p
               key={index}
               className="text-black text-center leading-7"
@@ -120,7 +170,7 @@ const RandCard: React.FC<{}> = () => {
             <div className="flex flex-row items-center icons gap-3">
               <button
                 className="m-3 flex gap-2 items-center"
-                onClick={() => handleShareClick(randomData, "sherCard")}
+                onClick={() => handleShareClick(randomData!, "sherCard")}
               >
                 <FontAwesomeIcon icon={faShare} style={{ color: "#984A02" }} />
                 <p>Share this</p>
