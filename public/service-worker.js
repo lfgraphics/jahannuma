d585996af94548259f8b0d7c9d92fa9e7a0295ef
Taha Kazmi <tahakazmi143@gmail.com>
2023-12-17 01:03:14 +0530

    self if not defined err fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,23 +1,24 @@
-// public/service-worker.js
-
 const CACHE_NAME = 'jahannuma';
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                '/',
-                'logo.png',
-                'manifest.json',
-            ]);
-        })
-    );
-});
+// Check if `self` is defined (browser environment) before using it
+if (typeof self !== 'undefined') {
+    self.addEventListener('install', (event) => {
+        event.waitUntil(
+            caches.open(CACHE_NAME).then((cache) => {
+                return cache.addAll([
+                    '/',
+                    'logo.png',
+                    'manifest.json',
+                ]);
+            })
+        );
+    });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
+    self.addEventListener('fetch', (event) => {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            })
+        );
+    });
+}
