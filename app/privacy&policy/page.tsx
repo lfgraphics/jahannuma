62c27e28e145fde8026b5ddc0e525a9538530116
Taha Kazmi <tahakazmi143@gmail.com>
2024-01-12 08:52:33 +0530

    added 4 required pages with comingsoon page for razorpay

---
+++ app/privacy&policy/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComingSoon from '../Components/Commingsoon'
+
+const page = () => {
+  return (
+    <div>
+      <ComingSoon />
+    </div>
+  )
+}
+
+export default page
