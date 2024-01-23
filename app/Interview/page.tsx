89cc2eb66a1c7d5a983c961a10f38a960e2e8127
Taha Kazmi <tahakazmi143@gmail.com>
2024-01-24 03:16:57 +0530

    added intervies page

--- app/Interview/page.tsx
+++ app/Interview/page.tsx
@@ -1,12 +1,16 @@
-import React from "react";
-import Commingsoon from "../Components/Commingsoon";
+import Content from "./Content";
+export const metadata = {
+  metadataBase: new URL("https://jahan-numa.org/Bazmeurdu"),
+  title: "Interviews | Jahannuma",
+  description:
+    "This page contains Interview and podcast videos of contestents interviewed by jahan numa",
+  openGraph: {
+    images: ["https://jahannuma.vercel.app/metaImages/interview.jpg"],
+  },
+};
 
 const page = () => {
-  return (
-    <div>
-      <Commingsoon />
-    </div>
-  );
+  return <Content />;
 };
 
 export default page;
