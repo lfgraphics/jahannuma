fe5ca1f2e2f424fc6726bcb14ebda977334d0113
Taha Kazmi <96362790+lfgraphics@users.noreply.github.com>
2024-08-30 11:02:03 +0530

    bug fix fixed the navigation errors in /Shaer page

--- app/Shaer/[name]/page.js
+++ app/Shaer/[name]/page.js
@@ -7,12 +7,14 @@ import Nazmen from "@/app/Components/shaer/Nazmen"
 import Ashaar from "@/app/Components/shaer/Ashaar"
 import EBkooks from "@/app/Components/shaer/EBooks"
 import Rubai from "@/app/Components/shaer/Rubai"
+import ComponentsLoader from "@/app/Components/shaer/ComponentsLoader";
 import Link from 'next/link';
 import './shaer.css';
 
 const Page = ({ params }) => {
   const [data, setData] = useState([]);
-  const [activeNav, setActiveNav] = useState(''); 
+  const [activeNav, setActiveNav] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const initializeActiveNav = () => {
@@ -45,6 +47,7 @@ const Page = ({ params }) => {
 
         const records = result.records || [];
         setData(records[0].fields);
+        setLoading(false)
       } catch (error) {
         console.error(`Failed to fetch data: ${error}`);
       }
@@ -64,89 +67,92 @@ const Page = ({ params }) => {
   };
 
   return (
-    <div dir="rtl" className="flex flex-col">
-      <Intro data={data} ></Intro>
-      <div className="inner-navs w-full md:w-[80vw] flex flex-row gap-3 mb-4 border-b-2 self-center p-4 text-xl">
-        <div
-          className={`nav-item ${activeNav === 'تعارف' ? 'active' : ''} min-w-[40px] text-center transition-all ease-in-out duration-500 `}
-          onClick={() => handleNavClick('تعارف')}
-        >
-          <Link href={`${"/Shaer/" + params.name + "?tab=تعارف"}`} prefetch >
-            تعارف
-          </Link>
+    <>
+      {loading && <ComponentsLoader />}
+      {!loading && (<div dir="rtl" className="flex flex-col">
+        <Intro data={data} ></Intro>
+        <div className="inner-navs w-full md:w-[80vw] flex flex-row gap-3 mb-4 border-b-2 self-center p-4 text-xl">
+          <div
+            className={`nav-item ${activeNav === 'تعارف' ? 'active' : ''} min-w-[40px] text-center transition-all ease-in-out duration-500 `}
+            onClick={() => handleNavClick('تعارف')}
+          >
+            <Link href={`${"/Shaer/" + params.name + "?tab=تعارف"}`} prefetch >
+              تعارف
+            </Link>
+          </div>
+          {data.ghazlen && data.ghazlen === true && (
+            <div
+              className={`nav-item ${activeNav === 'غزلیں' ? 'active' : ''} min-w-[40px] text-center transition-all ease-in-out duration-500 `}
+              onClick={() => handleNavClick('غزلیں')}
+            >
+              <Link href={`${"/Shaer/" + params.name + "?tab=غزلیں"}`} prefetch >
+                غزلیں
+              </Link>
+            </div>
+          )}
+          {data.nazmen && data.nazmen === true && (
+            <div
+              className={`nav-item ${activeNav === 'نظمیں' ? 'active' : ''} min-w-[40px] text-center transition-all ease-in-out duration-500 `}
+              onClick={() => handleNavClick('نظمیں')}
+            >
+              <Link href={`${"/Shaer/" + params.name + "?tab=نظمیں"}`} prefetch >
+                نظمیں
+              </Link>
+            </div>
+          )}
+          {data.ashaar && data.ashaar === true && (
+            <div
+              className={`nav-item ${activeNav === 'اشعار' ? 'active' : ''} min-w-[40px] text-center transition-all ease-in-out duration-500 `}
+              onClick={() => handleNavClick('اشعار')}
+            >
+              <Link href={`${"/Shaer/" + params.name + "?tab=اشعار"}`} prefetch >
+                اشعار
+              </Link>
+            </div>
+          )}
+          {data.eBooks && data.eBooks === true && (
+            <div
+              className={`nav-item ${activeNav === 'ئی - بکس' ? 'active' : ''} min-w-[40px] text-center transition-all ease-in-out duration-500 `}
+              onClick={() => handleNavClick('ئی - بکس')}
+            >
+              <Link href={`${"/Shaer/" + params.name + "?tab=ئی - بکس"}`} prefetch >
+                ئی - بکس
+              </Link>
+            </div>
+          )}
+          {data.rubai && data.rubai === true && (
+            <div
+              className={`nav-item ${activeNav === 'رباعی' ? 'active' : ''} min-w-[40px] text-center transition-all ease-in-out duration-500 `}
+              onClick={() => handleNavClick('رباعی')}
+            >
+              <Link href={`${"/Shaer/" + params.name + "?tab=رباعی"}`} prefetch >
+                رباعی
+              </Link>
+            </div>
+          )}
+
         </div>
-        {data.ghazlen && data.ghazlen === true && (
-          <div
-            className={`nav-item ${activeNav === 'غزلیں' ? 'active' : ''} min-w-[40px] text-center transition-all ease-in-out duration-500 `}
-            onClick={() => handleNavClick('غزلیں')}
-          >
-            <Link href={`${"/Shaer/" + params.name + "?tab=غزلیں"}`} prefetch >
-              غزلیں
-            </Link>
-          </div>
+        {activeNav === "تعارف" && (
+          <Intro2 data={data} ></Intro2>
         )}
-        {data.nazmen && data.nazmen === true && (
-          <div
-            className={`nav-item ${activeNav === 'نظمیں' ? 'active' : ''} min-w-[40px] text-center transition-all ease-in-out duration-500 `}
-            onClick={() => handleNavClick('نظمیں')}
-          >
-            <Link href={`${"/Shaer/" + params.name + "?tab=نظمیں"}`} prefetch >
-              نظمیں
-            </Link>
-          </div>
+        {activeNav === "غزلیں" && (
+          <Ghazlen takhallus={data.takhallus}></Ghazlen>
         )}
-        {data.ashaar && data.ashaar === true && (
-          <div
-            className={`nav-item ${activeNav === 'اشعار' ? 'active' : ''} min-w-[40px] text-center transition-all ease-in-out duration-500 `}
-            onClick={() => handleNavClick('اشعار')}
-          >
-            <Link href={`${"/Shaer/" + params.name + "?tab=اشعار"}`} prefetch >
-              اشعار
-            </Link>
-          </div>
+        {activeNav === "نظمیں" && (
+          <Nazmen takhallus={data.takhallus}></Nazmen>
         )}
-        {data.eBooks && data.eBooks === true && (
-          <div
-            className={`nav-item ${activeNav === 'ئی - بکس' ? 'active' : ''} min-w-[40px] text-center transition-all ease-in-out duration-500 `}
-            onClick={() => handleNavClick('ئی - بکس')}
-          >
-            <Link href={`${"/Shaer/" + params.name + "?tab=ئی - بکس"}`} prefetch >
-              ئی - بکس
-            </Link>
-          </div>
+        {activeNav === "اشعار" && (
+          <Ashaar takhallus={data.takhallus} ></Ashaar>
         )}
-        {data.rubai && data.rubai === true && (
-          <div
-            className={`nav-item ${activeNav === 'رباعی' ? 'active' : ''} min-w-[40px] text-center transition-all ease-in-out duration-500 `}
-            onClick={() => handleNavClick('رباعی')}
-          >
-            <Link href={`${"/Shaer/" + params.name + "?tab=رباعی"}`} prefetch >
-              رباعی
-            </Link>
-          </div>
+        {activeNav === "ئی - بکس" && (
+          <EBkooks takhallus={data.takhallus} ></EBkooks>
+        )}
+        {activeNav === "رباعی" && (
+          <Rubai takhallus={data.takhallus} ></Rubai>
         )}
 
-      </div>
-      {activeNav === "تعارف" && (
-        <Intro2 data={data} ></Intro2>
-      )}
-      {activeNav === "غزلیں" && (
-        <Ghazlen takhallus={decodedName.trim()}></Ghazlen>
-      )}
-      {activeNav === "نظمیں" && (
-        <Nazmen takhallus={decodedName.trim()}></Nazmen>
-      )}
-      {activeNav === "اشعار" && (
-        <Ashaar takhallus={decodedName.trim()} ></Ashaar>
-      )}
-      {activeNav === "ئی - بکس" && (
-        <EBkooks takhallus={decodedName.trim()} ></EBkooks>
-      )}
-      {activeNav === "رباعی" && (
-        <Rubai takhallus={decodedName.trim()} ></Rubai>
-      )}
-
-    </div>
+      </div>)}
+    </>
   );
 };
 
